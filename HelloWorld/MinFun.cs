d434bb5ac1ff5c55e9c329b163715d8ccd582ab1
Ваня <ivanbeshka2002@gmail.com>
2020-09-18 14:01:46 +0500

    Initial commit

---
+++ HelloWorld/MinFun.cs
@@ -0,0 +1,29 @@
+﻿namespace HelloWorld
+{
+    public class MinFun
+    {
+
+        static void Main()
+        {
+            
+        }
+        
+        private static string GetMinX(int a, int b, int c)
+        {
+            double res;
+            if (a > 0)
+            {
+                res = -((double) b / (2 * a));
+                return res.ToString();
+            }
+
+            if (a == 0 && b == 0)
+            {
+                return "";
+            }
+
+            string e = "Impossible";
+            return e;
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main2()
+        {
+            Console.WriteLine(Decode("123"));
+        }
+
+        static string Decode(string stroke)
+        {
+            var parsedNumber = int.Parse(stroke.Replace(".", ""));
+            return parsedNumber > 1024 ? (parsedNumber - 1024).ToString() : parsedNumber.ToString();
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        }
+    }
+}
